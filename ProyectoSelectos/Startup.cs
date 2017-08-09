2f44fcb427188752bc3f2229b1eae7daf3a7f1e9
Cesar Naranjo <mmkcesar@hotmail.com>
2017-08-09 11:22:18 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoSelectos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoSelectos.Startup))]
+namespace ProyectoSelectos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
